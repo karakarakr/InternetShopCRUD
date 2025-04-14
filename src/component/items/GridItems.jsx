fdea446a836708f4caff9c8280a9389fc471d076
karakarakr <titan1234eti@gmail.com>
2025-04-14 10:59:35 +0300

    Added buy button and new Dialog

--- src/component/items/GridItems.jsx
+++ src/component/items/GridItems.jsx
@@ -2,18 +2,29 @@ import React, { useState } from 'react';
 import './GridItems.css';
 import ModalDialog from '../modal-dialog/ModalDialog';
 
-const ItemModalDialog = ({ item, onClick }) => {
+const ItemModalDialog = ({ item, onClick, buyClick }) => {
   return (
     <ModalDialog onClick={onClick}>
       <b>{item.id}. {item.name}</b>
       <p>Description: {item.description}</p>
       <p>Price: ${item.priceUSD}</p>
+      <p>Count: {item.count}</p>
+      <button onClick={buyClick}>Buy</button>
+    </ModalDialog>
+  );
+};
+
+const NotEnoughItemDialog = ({ item, onClick }) => {
+  return (
+    <ModalDialog onClick={onClick}>
+      <b>NOT Enough items!</b>
     </ModalDialog>
   );
 };
 
 const GridItems = ({ items }) => {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [dialogNotEnough, setDialogNotEnough] = useState(false);
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
@@ -21,6 +32,18 @@ const GridItems = ({ items }) => {
 
   const handleCloseModal = () => {
     setSelectedItem(null);
+  };
+
+  const buyItem = () => {
+    if (selectedItem.count <= 0) {
+      setDialogNotEnough(true);
+      return;
+    }
+    
+    setSelectedItem({
+      ...selectedItem, 
+      count: selectedItem.count - 1
+    });
   };
 
   return (
@@ -42,6 +65,13 @@ const GridItems = ({ items }) => {
         <ItemModalDialog
           item={selectedItem}
           onClick={handleCloseModal}
+          buyClick={() => buyItem()}
+        />
+      )}
+
+      {dialogNotEnough && (
+        <NotEnoughItemDialog
+          onClick={() => setDialogNotEnough(false)}
         />
       )}
     </>
